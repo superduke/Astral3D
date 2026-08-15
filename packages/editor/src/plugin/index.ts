f986b847c24324fa3a335ffd23e635382fb14a82
superduke <superduke1984@gmail.com>
2026-08-15 13:07:41 +0800

    register factory generator plugin

--- packages/editor/src/plugin/index.ts
+++ packages/editor/src/plugin/index.ts
@@ -1,5 +1,6 @@
 import GLTFHandler from "./glTFHandler/glTFHandler";
 import PointCloudReconstructor from "./pointCloudReconstructor/PointCloudReconstructor";
+import FactoryGenerator from "./factoryGenerator/FactoryGenerator";
 
 // 注册内置插件
 export const installBuiltinPlugin = (viewer) => {
@@ -7,4 +8,6 @@ export const installBuiltinPlugin = (viewer) => {
     viewer.modules.plugin.use(new GLTFHandler());
     // 语义化点云重建
     viewer.modules.plugin.use(new PointCloudReconstructor());
+    // 半导体园区参数化生成
+    viewer.modules.plugin.use(new FactoryGenerator());
 }
